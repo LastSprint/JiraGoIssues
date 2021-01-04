3c31b21bdcd993e086b8e616b8fd9bba4779d73c
Alexander Kravchenkov <a.a.kravchenkov@gmail.com>
2021-01-04 18:07:27 +0300

    add other images

--- models/UserReferenceEntity.go
+++ models/UserReferenceEntity.go
@@ -1,7 +1,10 @@
 package models
 
 type Avatars struct {
-	LargestUrl string `json:"48x48"`
+	LargestUrl  string `json:"48x48"`
+	MidUrl      string `json:"32x32"`
+	SmallUrl    string `json:"24x24"`
+	SmallestUrl string `json:"16x16"`
 }
 
 // UserReferenceEntity is short user entity
