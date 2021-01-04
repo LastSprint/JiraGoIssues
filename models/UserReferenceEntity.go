b888bec63662d9f9243d9f0f9b1219175cfcfe4a
Alexander Kravchenkov <a.a.kravchenkov@gmail.com>
2021-01-04 17:06:36 +0300

    add avatars to user reference

--- models/UserReferenceEntity.go
+++ models/UserReferenceEntity.go
@@ -1,8 +1,13 @@
 package models
 
+type Avatars struct {
+	LargestUrl string `json:"48x48"`
+}
+
 // UserReferenceEntity is short user entity
 type UserReferenceEntity struct {
-	DisplayName string `json:"displayName"`
-	Key         string `json:"key"`
-	Name        string `json:"name"`
+	DisplayName string  `json:"displayName"`
+	Key         string  `json:"key"`
+	Name        string  `json:"name"`
+	AvatarUrls  Avatars `json:"avatarUrls"`
 }
